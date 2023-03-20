aa50afdaa295ad70ec307415b3fba93fb026246e
Nick Garza <thenickgarza14@gmail.com>
2023-03-20 18:32:07 -0500

    setting up function recieve user input to search for repos

---
+++ assets/js/homepage.js
@@ -0,0 +1,13 @@
+let getUserRepos = function(user) {
+    let apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    fetch(apiUrl)
+    .then(function(response) {
+        response.json()
+        .then(function(data) {
+            console.log(data)
+        })
+    })
+};
+
+getUserRepos("thenickgarza");
